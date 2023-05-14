eeef0daf3d30506fe6689d6b1c47cd6e15e11856
Martin Kamír <martin.k.kamir@gmail.com>
2023-05-15 00:32:51 +0200

    add finished UI, WIP history and bookmarks

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,4 @@
 export const API_URL = 'https://api.github.com/users';
-export const API_TOKEN = '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';
 export const TIMEOUT_SEC = 10;
 export const INIT_USER = 'Octocat'
 export const HISTORY_LIMIT = 10;
