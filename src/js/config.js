5af6447bb2b320367fb95540cf16ee550e09574f
Martin Kamír <martin.k.kamir@gmail.com>
2023-05-18 23:29:06 +0200

    fix

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://api.github.com/users';
-export const API_TOKEN = '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';
+export const API_TOKEN = '';
 export const TIMEOUT_SEC = 10;
 export const INIT_USER = 'Octocat'
 export const HISTORY_LIMIT = 20;
