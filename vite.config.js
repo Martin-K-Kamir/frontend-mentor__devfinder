74a7f59b7d03ed77cda269dd318a5f4d01a85f72
Martin Kamír <martin.k.kamir@gmail.com>
2023-05-19 19:14:21 +0200

    test service worker

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,64 @@ import {defineConfig} from 'vite';
 
 export default defineConfig({
     plugins: [
-        VitePWA()
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            },
+            manifest: {
+                name: 'Search awesome developers',
+                short_name: 'devfinder',
+                description: 'Search users profiles from github.com',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: "assets/favicons/icon-72x72.png",
+                        sizes: "72x72",
+                        type: "image/png",
+                        purpose: "any"
+                    },
+                    {
+                        src: "assets/favicons/icon-96x96.png",
+                        sizes: "96x96",
+                        type: "image/png",
+                        purpose: "any"
+                    },
+                    {
+                        src: "assets/favicons/icon-128x128.png",
+                        sizes: "128x128",
+                        type: "image/png",
+                        purpose: "any"
+                    },
+                    {
+                        src: "assets/favicons/icon-144x144.png",
+                        sizes: "144x144",
+                        type: "image/png",
+                        purpose: "any"
+                    },
+                    {
+                        src: "assets/favicons/icon-152x152.png",
+                        sizes: "152x152",
+                        type: "image/png",
+                        purpose: "any"
+                    },
+                    {
+                        src: "assets/favicons/icon-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable"
+                    },
+                    {
+                        src: "assets/favicons/icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable"
+                    }
+                ]
+            }
+        })
     ]
 })
