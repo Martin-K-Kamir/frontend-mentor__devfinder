9a9dd0447e673bf88d8926b2e8dee4376de53552
Martin Kamír <martin.k.kamir@gmail.com>
2023-05-19 23:54:29 +0200

    test service worker

--- src/js/config.js
+++ src/js/config.js
@@ -4,4 +4,4 @@ export const TIMEOUT_SEC = 10;
 export const INIT_USER = 'Octocat'
 export const HISTORY_LIMIT = 20;
 export const SHOW_INSTALL_PROMPT_SEC = 1.5;
-export const SHOW_INSTALL_PROMPT_LIMIT = 1500;
+export const SHOW_INSTALL_PROMPT_LIMIT = 3;
